dc1540d8434f92e8e7756657a3da6fd6dd8aadb2
Vo Tan Sang <22130233@st.hcmuaf.edu.vn>
2026-03-07 11:26:21 +0700

    chore: setup environment variables, axios interceptors and sonner toast

--- front-end/frontend/src/shared/utils/axios.ts
+++ front-end/frontend/src/shared/utils/axios.ts
@@ -1,8 +1,16 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-    baseURL: "http://localhost:8080/headhunt",
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
         "Content-Type": "application/json",
     },
 });
+
+apiClient.interceptors.request.use((config)=> {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+        config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+})
