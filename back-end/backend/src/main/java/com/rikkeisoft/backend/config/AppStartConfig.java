ceb0585c4223e8a83d405103bd13eddfd12817a2
trungducnguyen4 <nguyentrungduc.forwork@gmail.com>
2026-03-10 15:09:28 +0700

    [DRAF] add:verify Ma so thue doanh nghiep hop le hay k

--- back-end/backend/src/main/java/com/rikkeisoft/backend/config/AppStartConfig.java
+++ back-end/backend/src/main/java/com/rikkeisoft/backend/config/AppStartConfig.java
@@ -13,6 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -63,5 +64,10 @@ public class AppStartConfig {
                 System.out.println("Admin account created!");
             }
         };
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
