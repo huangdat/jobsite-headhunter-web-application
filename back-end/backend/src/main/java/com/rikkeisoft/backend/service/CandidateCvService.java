a584300dbc74225c64f1a6d732d66b4f460f7c95
Nguyen Hoang Dat <datnhce180797@fpt.edu.vn>
2026-03-18 15:01:56 +0700

    create cv row in DB after register candidate

--- back-end/backend/src/main/java/com/rikkeisoft/backend/service/CandidateCvService.java
+++ back-end/backend/src/main/java/com/rikkeisoft/backend/service/CandidateCvService.java
@@ -3,6 +3,7 @@ package com.rikkeisoft.backend.service;
 import com.rikkeisoft.backend.model.dto.PagedResponse;
 import com.rikkeisoft.backend.model.dto.req.account.*;
 import com.rikkeisoft.backend.model.dto.req.cv.CandidateCvCreateReq;
+import com.rikkeisoft.backend.model.dto.req.cv.CandidateCvUpdateReq;
 import com.rikkeisoft.backend.model.dto.resp.account.AccountResp;
 import com.rikkeisoft.backend.model.dto.resp.cv.CandidateCvResp;
 import com.rikkeisoft.backend.model.entity.CandidateCv;
@@ -15,14 +16,8 @@ public interface CandidateCvService {
     CandidateCvResp getCvByCandidateId(String candidateId);
     CandidateCvResp getMyCv();
     CandidateCvResp createCandidateCv(CandidateCvCreateReq req);
-    AccountResp updateMyAccount(AccountUpdateReq req);
-    AccountResp updateStatus(String id, String status);
-
-    String changePassword(AccountChangePasswordreq req);
-
-    PagedResponse<AccountResp> searchAccounts(int page, int size, String keyword, String role, String status, String sort);
-
-    AccountResp createAccountHeadhunter(HeadhunterSignupReq req);
+    CandidateCvResp updateMyAccount(CandidateCvUpdateReq req);
+    CandidateCvResp updateStatus(String id, String status);
 
     AccountResp createAccountCollaborator(CollaboratorSignupReq req);
 
--- back-end/backend/src/main/java/com/rikkeisoft/backend/service/impl/CandidateCvServiceImpl.java
+++ back-end/backend/src/main/java/com/rikkeisoft/backend/service/impl/CandidateCvServiceImpl.java
@@ -1,19 +1,28 @@
 package com.rikkeisoft.backend.service.impl;
 
+import com.rikkeisoft.backend.model.dto.PagedResponse;
+import com.rikkeisoft.backend.model.dto.req.account.*;
+import com.rikkeisoft.backend.model.dto.req.cv.CandidateCvCreateReq;
+import com.rikkeisoft.backend.model.dto.resp.account.AccountResp;
+import com.rikkeisoft.backend.model.dto.resp.cv.CandidateCvResp;
 import com.rikkeisoft.backend.model.entity.CandidateCv;
 import com.rikkeisoft.backend.repository.CandidateCvRepo;
+import com.rikkeisoft.backend.service.CandidateCvService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Slf4j
 @RequiredArgsConstructor // Generates a constructor with required arguments for final fields.
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @Service
-public class CandidateCvServiceImpl {
+public class CandidateCvServiceImpl implements CandidateCvService {
     CandidateCvRepo candidateCvRepo;
+
 
 
 }
