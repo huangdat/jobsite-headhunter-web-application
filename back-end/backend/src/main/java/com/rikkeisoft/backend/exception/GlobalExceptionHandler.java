94085f6fba08056a42c3416f4cc546cfac270bba
Đinh Công Phúc <phuc.dinhcong813@gmail.com>
2026-03-19 23:20:20 +0700

    chore: implement centralized exception handling and I18n support(Spring resource bundle)
    - Create `AppException` for custom application logic errors
    - Configure resource bundle and application settings in `application.yaml`
    - Update `ErrorCode` to support localized message keys
    - Implement `Translator` utility for message localization
    - Refactor `GlobalExceptionHandler` and `JobControllerAdvice` to use centralized error handling
    - Update `AuthenticationController` to integrate with the new exception handling flow

--- back-end/backend/src/main/java/com/rikkeisoft/backend/exception/GlobalExceptionHandler.java
+++ back-end/backend/src/main/java/com/rikkeisoft/backend/exception/GlobalExceptionHandler.java
@@ -1,9 +1,9 @@
 package com.rikkeisoft.backend.exception;
 
+import com.rikkeisoft.backend.component.Translator;
 import com.rikkeisoft.backend.enums.ErrorCode;
 import com.rikkeisoft.backend.model.dto.APIResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.authorization.AuthorizationDeniedException;
@@ -16,89 +16,116 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    private final Translator translator;
+
+    public GlobalExceptionHandler(Translator translator) {
+        this.translator = translator;
+    }
+
     /**
      * Handles custom application exceptions.
-     * @param e
-     * @return ResponseEntity with APIResponse containing error code and message
+     *
+     * @param e the AppException
+     * @return ResponseEntity with APIResponse containing error status and translated message
      */
     @ExceptionHandler(value = AppException.class)
-    ResponseEntity<APIResponse> handlingAppException(AppException e) {
+    ResponseEntity<APIResponse<?>> handlingAppException(AppException e) {
         ErrorCode errorCode = e.getErrorCode();
-        APIResponse response = new APIResponse();
-        response.setStatus(errorCode.getHttpStatus());
-        response.setMessage(errorCode.getMessage());
-        return ResponseEntity.badRequest().body(response);
+        return ResponseEntity
+                .status(errorCode.getHttpStatus())
+                .body(buildAPIResponse(errorCode));
     }
 
     @ExceptionHandler(value = RuntimeException.class)
-    ResponseEntity<APIResponse> handlingRuntimeException(RuntimeException e) {
+    ResponseEntity<APIResponse<?>> handlingRuntimeException(RuntimeException e) {
         log.error("Unhandled RuntimeException: ", e);
-        APIResponse response = new APIResponse();
-        response.setStatus(ErrorCode.INTERNAL_SERVER_ERROR.getHttpStatus());
-        response.setMessage(ErrorCode.INTERNAL_SERVER_ERROR.getMessage());
-        return ResponseEntity.badRequest().body(response);
+        ErrorCode errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
+        return ResponseEntity
+                .status(errorCode.getHttpStatus())
+                .body(buildAPIResponse(errorCode));
     }
 
     /**
      * Handles authorization denied exceptions.
-     * @param ex
+     *
+     * @param ex the AuthorizationDeniedException
      * @return ResponseEntity with APIResponse containing error code and message
      */
     @ExceptionHandler(AuthorizationDeniedException.class)
     public ResponseEntity<APIResponse<?>> handlingAuthorizationDeniedException(AuthorizationDeniedException ex) {
-        APIResponse<?> resp = new APIResponse<>();
-        resp.setStatus(HttpStatus.FORBIDDEN);
-        resp.setMessage("Access Denied");
-        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(resp);
+        ErrorCode errorCode = ErrorCode.UNAUTHORIZED;
+        return ResponseEntity
+                .status(errorCode.getHttpStatus())
+                .body(buildAPIResponse(ErrorCode.UNAUTHORIZED));
     }
 
     /**
      * Handles HTTP message not readable exceptions, typically occurring when the request body is invalid or cannot be parsed.
-     * @param ex
-     * @return
+     *
+     * @param ex the HttpMessageNotReadableException
+     * @return ResponseEntity with APIResponse containing bad request status and specific cause
      */
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<APIResponse<?>> handlingHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
-        APIResponse<?> resp = new APIResponse<>();
-        resp.setStatus(HttpStatus.BAD_REQUEST);
-        resp.setMessage("Invalid request body: " + ex.getMostSpecificCause().getMessage());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(resp);
+        ErrorCode errorCode = ErrorCode.BAD_REQUEST;
+        // Using "Bad Request" localized message + specific cause
+        String baseMessage = translator.toLocale("error.bad_request" + ": " + ex.getMostSpecificCause().getMessage());
+        return ResponseEntity
+                .status(errorCode.getHttpStatus())
+                .body(
+                        APIResponse.builder()
+                                .status(errorCode.getHttpStatus())
+                                .message(baseMessage)
+                                .build()
+                );
     }
 
     /**
      * Handles validation exceptions.
-     * @param e
+     *
+     * @param e the MethodArgumentNotValidException
      * @return ResponseEntity with APIResponse containing error code and message
      */
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    ResponseEntity<APIResponse> handlingValidation(MethodArgumentNotValidException e) {
-
+    ResponseEntity<APIResponse<?>> handlingValidation(MethodArgumentNotValidException e) {
         String enumKey = e.getFieldError() != null ? e.getFieldError().getDefaultMessage() : "INVALID_KEY";
-        log.warn("Validation error: {}", enumKey);
 
         ErrorCode errorCode = ErrorCode.INVALID_KEY;
         try {
             errorCode = ErrorCode.valueOf(enumKey);
-        } catch (IllegalArgumentException ignored) {
+        } catch (IllegalArgumentException ex) {
+            log.warn("Validation error: {}", enumKey);
         }
 
-        APIResponse response = new APIResponse();
-        response.setStatus(errorCode.getHttpStatus());
-        response.setMessage(errorCode.getMessage());
-        return ResponseEntity.badRequest().body(response);
+        return ResponseEntity
+                .status(errorCode.getHttpStatus())
+                .body(buildAPIResponse(errorCode));
     }
-
 
     /**
      * Handles HTTP request method not supported exceptions.
-     * @param e
+     *
+     * @param e the HttpRequestMethodNotSupportedException
      * @return ResponseEntity with APIResponse containing error code and message
      */
     @ExceptionHandler(value = HttpRequestMethodNotSupportedException.class)
-    ResponseEntity<APIResponse> handlingHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
-        APIResponse response = new APIResponse();
-        response.setStatus(ErrorCode.METHOD_NOT_ALLOWED.getHttpStatus());
-        response.setMessage(ErrorCode.METHOD_NOT_ALLOWED.getMessage());
-        return ResponseEntity.status(405).body(response);
+    ResponseEntity<APIResponse<?>> handlingHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
+        ErrorCode errorCode = ErrorCode.METHOD_NOT_ALLOWED;
+        return ResponseEntity
+                .status(errorCode.getHttpStatus())
+                .body(buildAPIResponse(errorCode));
+    }
+
+    /**
+     * Handles the translation of the ErrorCode key and encapsulates the data into the APIResponse object.
+     *
+     * @param errorCode the ErrorCode to use
+     * @return a populated APIResponse
+     */
+    private APIResponse<?> buildAPIResponse(ErrorCode errorCode) {
+        return APIResponse.builder()
+                .status(errorCode.getHttpStatus())
+                .message(translator.toLocale(errorCode.getMessageKey()))
+                .build();
     }
 }
